87c8e68b4b595d1a9c08c1724821205b8bc6aa82
maxnicholson-coding <maxnic2378@gmail.com>
2026-09-09 10:15:52 -0500

    Create Student class with name and ID properties

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    //initialize varaibles
+    private String name;
+    private int studentID;
+    public Student(String name, int id) {
+        //set varaibles
+        this.name = name;
+        studentID = id;
+    }
+    public String getName() { //returns the student's name
+        return name;
+    }
+    public int getID() { // returns the student's ID number
+        return studentID;
+    }
+}
