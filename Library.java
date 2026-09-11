fd093fb1d9c9be228bbd2c5c7f8cee18256255d1
maxnicholson-coding <maxnic2378@gmail.com>
2026-09-11 17:23:29 -0500

    Added printing methods and fixed null pointer bugs

--- Library.java
+++ Library.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 public class Library {
     //initialize varaibles
     private String name;
-    public ArrayList<Book> allBooks;
-    public ArrayList<Student> allStudents;
-    public ArrayList<Loan> allLoans;
+    public ArrayList<Book> allBooks = new ArrayList<>();
+    public ArrayList<Student> allStudents = new ArrayList<>();
+    public ArrayList<Loan> allLoans = new ArrayList<>();
     public Library(String name) {
         this.name = name;
     }
@@ -28,6 +28,9 @@ public class Library {
         //sets loan status to inactive
         loan.setStatus("Inactive");
     }
+    public String getName() { //returns the name of the library
+        return name;
+    }
     public ArrayList getAllBooks() { //returns all books in the library
         return (allBooks);
     }
@@ -36,5 +39,38 @@ public class Library {
     }
     public ArrayList getAllLoans() { //returns all loans ever made
         return (allLoans);
+    }
+    public void printAllBooks() { //prints all books
+        //checks if there are no books
+        if (allBooks.isEmpty()) {
+            System.out.println("There are currently no books.");
+        } else {
+            for (int i = 0; i < allBooks.size(); i++) {
+                System.out.println(allBooks.get(i));
+                System.out.println();
+            }
+        }
+    }
+    public void printAllStudents() { //prints all students
+        //checks if there are no students
+        if (allStudents.isEmpty()) {
+            System.out.println("There are currently no students.");
+        } else {
+            for (int i = 0; i < allStudents.size(); i++) {
+                System.out.println(allStudents.get(i));
+                System.out.println();
+            }
+        }
+    }
+    public void printAllLoans() { //prints all loans
+        //checks if there are no loans
+        if (allLoans.isEmpty()) {
+            System.out.println("There are currently no loans.");
+        } else {
+            for (int i = 0; i < allLoans.size(); i++) {
+                System.out.println(allLoans.get(i));
+                System.out.println();
+            }
+        }
     }
 }
